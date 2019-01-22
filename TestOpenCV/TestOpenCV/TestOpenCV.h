af60575d6632e0c308ef340b66b0934221de4660
Mephistofer <Mephistof@163.com>
2019-01-22 14:38:23 +0800

    Create

---
+++ TestOpenCV/TestOpenCV/TestOpenCV.h
@@ -0,0 +1,32 @@
+﻿
+// TestOpenCV.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTestOpenCVApp:
+// 有关此类的实现，请参阅 TestOpenCV.cpp
+//
+
+class CTestOpenCVApp : public CWinApp
+{
+public:
+	CTestOpenCVApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestOpenCVApp theApp;
